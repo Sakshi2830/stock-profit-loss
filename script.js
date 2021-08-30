d9336f0d5c1159874e1640d68f6e01842a3a346f
Sakshi2830 <60890494+Sakshi2830@users.noreply.github.com>
2021-08-30 21:18:59 +0530

    limiting the decimal values

--- script.js
+++ script.js
@@ -8,10 +8,10 @@ var outputBox = document.querySelector("#output-box");
 function calculateLossAndProfit(initial, quantity, current){
     if(initial > current){
         var loss = (initial - current) * quantity
-        var lossPercentage = (loss / initial) * 100
+        var lossPercentage = ((loss / initial) * 100).toFixed(2)
         outputBox.innerText = `Hey the loss is ${loss} and percentage is ${lossPercentage}`
     }else if(current > initial){
-        var profit = (current - initial ) * quantity
+        var profit = ((current - initial ) * quantity).toFixed(2)
         var profitPercentage = (profit / initial) * 100
        outputBox.innerText =`Hey the profit is ${profit} and percentage is ${profitPercentage}`
 
